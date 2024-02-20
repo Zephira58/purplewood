7babaf0706a679ae35a79bfcad6cbdbfcd355628
Aviion Gibbs <xanthus58@protonmail.com>
2024-02-20 14:46:22 +1030

    Add: Unix timestamp
    Made life a bit easier by automaticly fetching the time the user runs the recruit command, also allows us to slot in 1 more variable, in this case if the user is trained or not.

--- src/commands/recruit.rs
+++ src/commands/recruit.rs
@@ -2,19 +2,28 @@ use serenity::builder::*;
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 use serenity::utils::CreateQuickModal;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<(), serenity::Error> {
+    // Get the current time as a Duration since the Unix epoch
+    let duration = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards!");
+
+    // Convert the Duration to Unix time (seconds since the Unix epoch)
+    let unix_time = duration.as_secs();
+
     let modal = CreateQuickModal::new("Who did we recruit?")
         .timeout(std::time::Duration::from_secs(600))
         .short_field("Recruit Name")
         .short_field("Your Discord name")
         .short_field("SteamID")
-        .short_field("Date Recruited")
-        .short_field("In the discord?");
+        .short_field("Are they in the discord?")
+        .short_field("Have they been trained?");
     let response = interaction.quick_modal(ctx, modal).await?.unwrap();
 
     let inputs = response.inputs;
-    let (recruit_name, recruiter_name, recruit_steamid, recruited_date, in_discord) =
+    let (recruit_name, recruiter_name, recruit_steamid, in_discord, is_trained) =
         (&inputs[0], &inputs[1], &inputs[2], &inputs[3], &inputs[4]);
 
     response
@@ -22,7 +31,7 @@ pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<(), 
         .create_response(
             ctx,
             CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content(
-                format!("**Recruited:** {recruit_name} ({recruit_steamid})\n**Recruiter:** {recruiter_name}\n**Date Recruited:** {recruited_date}\n**In iscord:** {in_discord}"),
+                format!("**Recruited:** {recruit_name} ({recruit_steamid})\n**Recruiter:** {recruiter_name}\n**Date Recruited:** <t:{:?}>\n**In Discord**: {in_discord}\n**Is Trained:** {is_trained}", unix_time),
             )),
         )
         .await?;
@@ -34,4 +43,4 @@ pub fn register() -> CreateCommand {
 }
 
 //"**Recruited:** Orangefin (STEAM_0:1:238532748)\n**Recruiter:** 𝖲𝖺𝗈𝗂𝗋𝗌𝖾\n**Date Recruited:** 24/02/20\n**In discord:** True\n**Trained:** True"
-//format!("**Recruited:** {recruit_name} ({recruit_steamid})\n**Recruiter:** {recruiter_name}\n**Date Recruited:** {recruited_date}\n**In discord:** {in_discord}\n**Trained:** {is_trained}"),
+//format!("**Recruited:** {recruit_name} ({recruit_steamid})\n**Recruiter:** {recruiter_name}\n**Date Recruited:** {recruited_date}\n**In discord:** {is_trained}\n**Trained:** {is_trained}"),
