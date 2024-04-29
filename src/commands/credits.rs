38ce354902466ffa4f6383406832290fe5023d68
Stella Gibbs <business@zephira.uk>
2024-04-30 09:07:54 +0930

    Fix: Updated email address in credits function

--- src/commands/credits.rs
+++ src/commands/credits.rs
@@ -2,7 +2,7 @@ use self_update::cargo_crate_version;
 use serenity::builder::CreateCommand;
 
 pub fn run() -> String {
-    format!("Heya my name is Zephira and i made this small discord bot to help out my friends on their discord server as a better solution to their ingame recruitment system.\nYou can find more of my projects here https://zephira.uk/ alongside my socials if you wish to get in contact ♥\n ```yaml\nBotInfo:\n  Version: {:?}\n  Owner: Zephira\n  Email: Zephira58@protonmail.com\n  License: GNU General Public License v3.0\n  Repo: https://github.com/Zephira58/purplewood\n  ```", cargo_crate_version!()).to_string()
+    format!("Heya my name is Zephira and i made this small discord bot to help out my friends on their discord server as a better solution to their ingame recruitment system.\nYou can find more of my projects here https://zephira.uk/ alongside my socials if you wish to get in contact ♥\n ```yaml\nBotInfo:\n  Version: {:?}\n  Owner: Zephira\n  Email: business@zephira.uk\n  License: GNU General Public License v3.0\n  Repo: https://github.com/Zephira58/purplewood\n  ```", cargo_crate_version!()).to_string()
 }
 
 pub fn register() -> CreateCommand {
